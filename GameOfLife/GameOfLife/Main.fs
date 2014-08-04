538e6906e3a03461ffa8e38e413b1c9bb9f4f1b4
NickLydon <nickkell@gmail.com>
2014-08-04 22:57:49 +0100

    Added c# forms app
    Added forms app, which has buttons to flick between iterations of the
    game of life

---
+++ GameOfLife/GameOfLife/Main.fs
@@ -0,0 +1,137 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open System
+open System.Collections.Generic
+
+module GameOfLife =
+
+    type State = 
+        | Alive
+        | Dead
+
+    type Cell = 
+        { state : State
+          position : int * int
+          neighbours : Cell list }
+
+    type console = System.Console
+
+    type Game() =
+
+        let cache = Dictionary<int, Cell list list>()
+
+        let gameLoop state = 
+            let stepCell (cell : Cell) = 
+                let liveNeighbours = 
+                    cell.neighbours
+                    |> List.filter (fun c -> c.state = Alive)
+                    |> List.length
+                match cell.state with
+                | Dead -> 
+                    if liveNeighbours = 3 then { cell with state = Alive }
+                    else cell
+                | Alive -> 
+                    if (liveNeighbours < 2 || liveNeighbours > 3) then { cell with state = Dead }
+                    else cell
+        
+            let withNeighbouringCells initialCells = 
+                initialCells 
+                |> List.map 
+                       (fun row -> 
+                       row 
+                       |> List.map 
+                              (fun cell -> 
+                              let neighbourCoordinates (x, y) = 
+                                  seq { 
+                                      for i in (x - 1)..(x + 1) do
+                                          for j in (y - 1)..(y + 1) do
+                                              if (i, j) <> (x, y) then yield (i, j)
+                                  }
+                              { cell with neighbours = 
+                                              initialCells
+                                              |> List.collect id
+                                              |> List.filter 
+                                                     (fun c -> 
+                                                     neighbourCoordinates (cell.position) 
+                                                     |> Seq.exists (fun p -> p = c.position)) }))
+        
+            let iteration = withNeighbouringCells state |> List.map (fun row -> row |> List.map stepCell)
+        
+            iteration
+    
+        let initialConfig = 
+            let pulsar = 
+                let topHalf = 
+                    let allDead = 
+                        [ for i in 1..15 -> Dead ]
+                    [ allDead
+                      [ Dead; Dead; Dead; Alive; Alive; Alive; Dead; Dead; Dead; Alive; Alive; Alive; Dead; Dead; Dead ]
+                      allDead
+                  
+                      [ Dead; Alive; Dead; Dead; Dead; Dead; Alive; Dead; Alive; Dead; Dead; Dead; Dead; Alive; Dead ]
+                  
+                      [ Dead; Alive; Dead; Dead; Dead; Dead; Alive; Dead; Alive; Dead; Dead; Dead; Dead; Alive; Dead ]
+                  
+                      [ Dead; Alive; Dead; Dead; Dead; Dead; Alive; Dead; Alive; Dead; Dead; Dead; Dead; Alive; Dead ]
+                      [ Dead; Dead; Dead; Alive; Alive; Alive; Dead; Dead; Dead; Alive; Alive; Alive; Dead; Dead; Dead ] ]
+                topHalf
+                |> List.append 
+                       [ [ Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; Dead; 
+                           Dead ] ]
+                |> List.append (topHalf |> List.rev)
+            pulsar
+    
+        let initialCells = 
+            initialConfig |> List.mapi (fun x row -> 
+                                 row |> List.mapi (fun y state -> 
+                                            { state = state
+                                              position = (x, y)
+                                              neighbours = [] }))
+    
+    
+        
+        member x.Play() =
+            Seq.unfold(fun state ->        
+                Some(state, gameLoop state)
+            ) initialCells
+
+        member x.Play numberOfIterations =
+            (0, initialCells)
+            |> Seq.unfold(fun (number, state) ->
+                cache.[number] <- state
+                let incremented = number + 1
+                if number > numberOfIterations
+                then None
+                else
+                    Some(state, (incremented, if cache.ContainsKey incremented then cache.[incremented] else gameLoop state))
+            )
+            |> Seq.last 
+            
+
+    [<EntryPoint>]
+    let main argv = 
+
+        let game = Game()
+    
+        let draw iteration =
+            let writeline() = console.WriteLine()
+            for row in iteration do
+                for cell in row do
+                    console.Write(if cell.state = Alive then 'O'
+                                    else ' ')
+                writeline()
+            for _ in 1 .. 6 do writeline()
+    
+    
+        if Array.empty = argv 
+        then game.Play() |> Seq.iter draw
+        else            
+            let (success,parsed) = Int32.TryParse(argv.[0])
+            if success 
+            then 
+                game.Play parsed |> draw 
+                Console.ReadLine() |> ignore
+ 
+
+        0 // return an integer exit code
---
+++ GameOfLife/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+        private readonly Main.GameOfLife.Game game = new Main.GameOfLife.Game();
+
+        private readonly IEnumerable<Tuple<Tuple<int, int>, Label>> cells =
+            Enumerable.Range(0, 15)
+                .Select(x => Enumerable.Range(0, 15)
+                    .Select(y =>
+                    {
+                        var label = new Label();
+
+                        label.Text = "X";
+
+                        label.Left = x * 17;
+                        label.Top = y * 17;
+                        label.Width = 17;
+                        label.Height = 17;
+
+                        return Tuple.Create(Tuple.Create(x,y), label);
+                    }))
+                .SelectMany(x => x)
+                .ToArray();
+
+        private int _iterations = 0;
+        private int Iterations
+        {
+            get
+            {
+                return _iterations;
+            }
+            set
+            {
+                if(value > 0 && value < int.MaxValue)
+                {
+                    _iterations = value;
+                    
+                    iterationLbl.Text = value.ToString();
+                    
+                    var played = game.Play(value);
+
+                    foreach (var row in played)
+                    {
+                        foreach (var column in row)
+                        {
+                            cells.Single(x =>
+                                x.Item1.Item1 == column.position.Item1 &&
+                                x.Item1.Item2 == column.position.Item2).Item2.Text = column.state == Main.GameOfLife.State.Alive ? "O" : "X";
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Controls.AddRange(cells.Select(x => x.Item2).Cast<Control>().ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Iterations--;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Iterations++;
+        }
+    }
+}
